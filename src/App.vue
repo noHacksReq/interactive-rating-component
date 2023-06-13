8dde33c24d9ca07f8261816c8f286b6d6091c94d
your name <crownroyalsuperslut@yahoo.com>
2023-06-13 11:00:56 -0400

    adding styles to front of card

--- src/App.vue
+++ src/App.vue
@@ -18,13 +18,15 @@ export default {
 
 <style>
 #app {
+  height: 100%;
+  width: 99%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-  background-color:  hsl(216, 12%, 54%);
+  background-color:  hsl(216, 12%, 8%);
   display: flex;
   align-items: center;
   justify-content: center;
