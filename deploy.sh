5ff68b63e9e4a2e8ec6bd1960591b9b4d711789e
your name <crownroyalsuperslut@yahoo.com>
2023-06-21 15:50:04 -0400

    code cleanup

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
- git push -f git@github.com:<noHacksReq>/<noHacksReq>.github.io.git main
+ git push -f git@github.com:noHacksReq/noHacksReq.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:<noHacksReq>/<interactive-rating-component>.git main:gh-pages
+ git push -f git@github.com:noHacksReq/interactive-rating-component.git main:gh-pages
 
 cd -
